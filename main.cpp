d950c4bb959c7ce6960cd0ebf645f98891e6c667
cpisz-posit <christopherpisz@posit.co>
2025-01-11 01:13:51 +0000

    find package soci

--- main.cpp
+++ main.cpp
@@ -2,12 +2,22 @@
 #include <boost/thread.hpp>
 #include <sqlite3.h>
 
+#include <iostream>
+
 int main()
 {
     boost::thread thread;
 
     sqlite3 *database = nullptr;
     int result = sqlite3_open("file:testdb.db", &database);
+    
+    if(result)
+    {
+        std::cerr << "Could not open sqlite db" << sqlite3_errmsg(database) << std::endl;
+        sqlite3_close(database);
+        return 1;
+    }
+
     sqlite3_close(database);
 
     return 0;
