31ac02cd6b12cabf83f131bfe9587b4c7fd368b2
cpisz-posit <christopher.pisz@posit.co>
2025-01-13 12:10:26 -0600

    sql statement syntax for soci found

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 
 int main()
 {
+    // Did boost link ok?
     boost::thread thread;
 
+    // Did sqlite3 link ok?
     sqlite3 *database = nullptr;
     int result = sqlite3_open("file:testdb.db", &database);
     
@@ -21,16 +23,42 @@ int main()
 
     sqlite3_close(database);
 
+    // Great, let's use soci
+
+    // Make a table of existing data
     try
     {
         soci::session sql("sqlite3", "file:testdb.db");
+
+        sql << "DROP TABLE IF EXISTS licensed_users";
+        
+        sql <<
+            "CREATE TABLE licensed_users(" 
+            "user_name text NOT NULL,"
+            "locked boolean NOT NULL DEFAULT 0,"
+            "last_sign_in text NOT NULL,"
+            "is_admin boolean NOT NULL DEFAULT 0,"
+            "user_id integer NOT NULL DEFAULT -1,"
+            "aws_role_arn text,"
+            "aws_role_session_name text,"
+            "id_token text,"
+            "refresh_token text,"
+            "token_expiry text,"
+            "id integer PRIMARY KEY,"
+            "created TEXT,"
+            "last_modified TEXT,"
+            "version TEXT,"
+            "email TEXT,"
+            "display_name TEXT,"
+            "posix_name TEXT,"
+            "shadow TEXT,"
+            "homedir TEXT,"
+            "active BOOLEAN NOT NULL DEFAULT 1)";
     }
     catch(const soci::soci_error & e)
     {
         std::cerr << e.what() << '\n';
     }
-
-    
 
     return 0;
 }
