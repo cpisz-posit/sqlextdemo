db087e0f8c937a6fb7c1b16d93cd86b89aa4b14d
cpisz-posit <christopher.pisz@posit.co>
2025-01-23 20:41:28 +0000

    update extension from rstudio-pro repo changes

--- sqlite_extensions/uuidext.cpp
+++ sqlite_extensions/uuidext.cpp
@@ -1,5 +1,6 @@
 /*
-** 2019-10-23
+** The source code in this file was based upon https://sqlite.org/src/file/ext/misc/uuid.c and edited
+** The original license follows:
 **
 ** The author disclaims copyright to this source code.  In place of
 ** a legal notice, here is a blessing:
@@ -10,55 +11,17 @@
 **
 ******************************************************************************
 **
-** This SQLite extension implements functions that handling RFC-4122 UUIDs
+** This SQLite extension implements functions that handle RFC-4122 UUIDs
 ** Three SQL functions are implemented:
 **
 **     uuid()        - generate a version 4 UUID as a string
 **     uuid_str(X)   - convert a UUID X into a well-formed UUID string
 **     uuid_blob(X)  - convert a UUID X into a 16-byte blob
-**
-** The output from uuid() and uuid_str(X) are always well-formed RFC-4122
-** UUID strings in this format:
-**
-**        xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
-**
-** All of the 'x', 'M', and 'N' values are lower-case hexadecimal digits.
-** The M digit indicates the "version".  For uuid()-generated UUIDs, the
-** version is always "4" (a random UUID).  The upper three bits of N digit
-** are the "variant".  This library only supports variant 1 (indicated
-** by values of N between '8' and 'b') as those are overwhelming the most
-** common.  Other variants are for legacy compatibility only.
-**
-** The output of uuid_blob(X) is always a 16-byte blob.  The UUID input
-** string is converted in network byte order (big-endian) in accordance
-** with RFC-4122 specifications for variant-1 UUIDs.  Note that network
-** byte order is *always* used, even if the input self-identifies as a
-** variant-2 UUID.
-**
-** The input X to the uuid_str() and uuid_blob() functions can be either
-** a string or a BLOB.  If it is a BLOB it must be exactly 16 bytes in
-** length or else a NULL is returned.  If the input is a string it must
-** consist of 32 hexadecimal digits, upper or lower case, optionally
-** surrounded by {...} and with optional "-" characters interposed in the
-** middle.  The flexibility of input is inspired by the PostgreSQL
-** implementation of UUID functions that accept in all of the following
-** formats:
-**
-**     A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11
-**     {a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11}
-**     a0eebc999c0b4ef8bb6d6bb9bd380a11
-**     a0ee-bc99-9c0b-4ef8-bb6d-6bb9-bd38-0a11
-**     {a0eebc99-9c0b4ef8-bb6d6bb9-bd380a11}
-**
-** If any of the above inputs are passed into uuid_str(), the output will
-** always be in the canonical RFC-4122 format:
-**
-**     a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11
-**
-** If the X input string has too few or too many digits or contains
-** stray characters other than {, }, or -, then NULL is returned.
+******************************************************************************
 */
+
 #include "sqlite_extensions/uuidext.hpp"
+SQLITE_EXTENSION_INIT1
 
 #include <cassert>
 #include <cstring>
@@ -68,12 +31,11 @@
 # define SQLITE_ASCII 1
 #endif
 
-
 static const char * ERR_MSG_MALFORMED = "UUID input param was malformed";
 
 
 /*
-* Translates an single byte of an integer to hex.
+* Translates a single byte of an integer to hex.
 * This routine only works if byte really is a valid hexadecimal character:  0..9a..fA..F
 */
 static unsigned char sqlite3UuidHexToInt(int byte)
@@ -114,34 +76,40 @@ static void sqlite3UuidBlobToStr(const unsigned char * bytes, unsigned char * re
 }
 
 /*
-** Attempt to parse a zero-terminated input string zStr into a binary
-** UUID.  Return 0 on success, or non-zero if the input string is not
-** parsable.
+* Parses a zero-terminated input string into a binary UUID
+* Returns 0 on success, or non-zero if the input string is not parsable
 */
-static int sqlite3UuidStrToBlob(const unsigned char *zStr,   /* Input string */
-                                unsigned char *aBlob         /* Write results here */)
+static int sqlite3UuidStrToBlob(const unsigned char * guidAsText, unsigned char * out)
 {
-  int i;
-  
-  if( zStr[0]=='{' )
-    zStr++;
+   if( guidAsText[0]=='{' )
+   {
+      ++guidAsText;
+   }
 
-  for(i=0; i<16; i++)
-  {
-    if( zStr[0]=='-' )
-        zStr++;
-    if( isxdigit(zStr[0]) && isxdigit(zStr[1]) )
-    {
-        aBlob[i] = (sqlite3UuidHexToInt(zStr[0])<<4) + sqlite3UuidHexToInt(zStr[1]);
-        zStr += 2;
-    }
-    else
-    {
-      return 1;
-    }
-  }
-  if( zStr[0]=='}' ) zStr++;
-  return zStr[0]!=0;
+   for(size_t i = 0; i < 16; ++i)
+   {
+      if( guidAsText[0]=='-' )
+      {
+         ++guidAsText;
+      }
+      
+      if( isxdigit(guidAsText[0]) && isxdigit(guidAsText[1]) )
+      {
+         out[i] = (sqlite3UuidHexToInt(guidAsText[0]) << 4) + sqlite3UuidHexToInt(guidAsText[1]);
+         guidAsText += 2;
+      }
+      else
+      {
+         return 1;
+      }
+   }
+
+   if( guidAsText[0]=='}' )
+   {
+      ++guidAsText;
+   }
+
+   return guidAsText[0] != 0;
 }
 
 /*
@@ -178,7 +146,14 @@ static void sqlite3UuidInputToBlob(sqlite3_value * value, unsigned char * out)
 }
 
 /* 
-* Implementation of uuid() sql function we are adding to sqlite
+* Implementation of the uuid() sql function we are adding to sqlite
+* The output of calling uuid_str() in sql will be a well-formed RFC-4122 UUID strings in this format:
+*
+*    xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
+*
+* All of the 'x', 'M', and 'N' values are lower-case hexadecimal digits. The M digit indicates the "version".  For uuid()-generated UUIDs, the
+* version is always "4" (a random UUID).  The upper three bits of N digit are the "variant".  This library only supports variant 1 (indicated by
+* values of N between '8' and 'b') as those are overwhelming the most common.  Other variants are for legacy compatibility only.
 */
 static void sqlite3UuidFunc(sqlite3_context * context, int argc, sqlite3_value ** argv)
 {
@@ -196,7 +171,30 @@ static void sqlite3UuidFunc(sqlite3_context * context, int argc, sqlite3_value *
 }
 
 /* 
-* Implementation of the SQL function uuid_str()
+* Implementation of the uuid_str() we are adding to sqlite
+*
+* The input value can be either a string or a BLOB. 
+*
+* If it is a BLOB it must be exactly 16 bytes in length and is converted in network byte order (big-endian) in accordance 
+* with RFC-4122 specifications for variant-1 UUIDs. Network byte order is always used, even if the input self-identifies as
+* a variant-2 UUID.
+*
+* If the input is a string it must consist of 32 hexadecimal digits, upper or lower case, optionally surrounded by {...} and 
+* with optional "-" characters interposed in the middle. The flexibility of input is inspired by the PostgreSQL implementation 
+* of UUID functions that accept in all of the following formats:
+*     A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11
+*     {a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11}
+*     a0eebc999c0b4ef8bb6d6bb9bd380a11
+*     a0ee-bc99-9c0b-4ef8-bb6d-6bb9-bd38-0a11
+*     {a0eebc99-9c0b4ef8-bb6d6bb9-bd380a11}
+*
+* The output will be a well-formed RFC-4122 UUID strings in this format:
+*
+*    xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
+*
+* All of the 'x', 'M', and 'N' values are lower-case hexadecimal digits. The M digit indicates the "version".  For uuid()-generated UUIDs, the
+* version is always "4" (a random UUID).  The upper three bits of N digit are the "variant".  This library only supports variant 1 (indicated by
+* values of N between '8' and 'b') as those are overwhelming the most common.  Other variants are for legacy compatibility only.
 */
 static void sqlite3UuidStrFunc(sqlite3_context * context, int argc, sqlite3_value ** argv)
 {
@@ -217,7 +215,24 @@ static void sqlite3UuidStrFunc(sqlite3_context * context, int argc, sqlite3_valu
 }
 
 /* 
-* Implementation of the SQL function uuid_blob() 
+* Implementation of the uuid_blob() function we are adding to sqlite
+*
+* The input value can be either a string or a BLOB. 
+*
+* If it is a BLOB it must be exactly 16 bytes in length and is converted in network byte order (big-endian) in accordance 
+* with RFC-4122 specifications for variant-1 UUIDs. Network byte order is always used, even if the input self-identifies as
+* a variant-2 UUID.
+*
+* If the input is a string it must consist of 32 hexadecimal digits, upper or lower case, optionally surrounded by {...} and 
+* with optional "-" characters interposed in the middle. The flexibility of input is inspired by the PostgreSQL implementation 
+* of UUID functions that accept in all of the following formats:
+*     A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11
+*     {a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11}
+*     a0eebc999c0b4ef8bb6d6bb9bd380a11
+*     a0ee-bc99-9c0b-4ef8-bb6d-6bb9-bd38-0a11
+*     {a0eebc99-9c0b4ef8-bb6d6bb9-bd380a11}
+*
+* The output will always be a 16-byte blob.
 */
 static void sqlite3UuidBlobFunc(sqlite3_context * context, int argc, sqlite3_value ** argv)
 {
@@ -262,3 +277,7 @@ int sqlite3_uuid_init(sqlite3 * db, char ** pzErrMsg, const sqlite3_api_routines
 
     return returnCode;
 }
+
+
+
+
